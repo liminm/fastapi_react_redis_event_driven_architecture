bfdae4bf9be594b7d170a77e9a96956c7ee5b187
liminm <liminmalek@gmail.com>
2023-12-25 01:29:10 +0100

    added functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 import json
 
+import consumers
 
 app = FastAPI()
 
@@ -38,10 +39,29 @@ class Event(HashModel):
         database = redis
 
 
+@app.get('/deliveries/{pk}/status')
+async def get_state(pk: str):
+    state = redis.get(f'delivery:{pk}')
+
+    if state is not None:
+        return json.loads(state)
+    return {}
+
+
+
+
+
 @app.post('/deliveries/create')
 async def create(request: Request):
     body = await request.json()
     delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
     event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
-    return event
+    state = consumers.create_delivery({}, event)
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+    return state
 
+
+@app.post('/event')
+async def dispatch(request: Request):
+    body = await request.json()
+    
